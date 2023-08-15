6716dc9cc49e957f5379c66583abe090d9ae42ac
Abduljeleel Bello <bellojeleel@yahoo.com>
2023-08-15 08:00:31 -0700

    Structure for dogs file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - A struct that create a dog file
+ *
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ *
+ * Desc: Poppy struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
