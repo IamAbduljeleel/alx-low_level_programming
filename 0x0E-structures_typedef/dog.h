807e3c4c916cd863b8bd5e8eb677215553fc0d7d
Abduljeleel Bello <bellojeleel@yahoo.com>
2023-08-15 12:57:15 -0700

    Change the name of struct using typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,18 +10,14 @@
  *
  * Desc: Poppy struct
  */
-/**
- * @dog_t - Typedef New name for struct dog
- */
 
-typedef struct dog dog_t;
+typedef struct dog
 
-struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
