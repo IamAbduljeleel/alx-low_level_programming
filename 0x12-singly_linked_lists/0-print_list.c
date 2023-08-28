b7c175af0ab290eae728c2a6a90831c06a1a2e43
Abduljeleel Bello <bellojeleel@yahoo.com>
2023-08-28 04:44:02 -0700

    The function that prints all the elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 	for (n = 1; h->next != NULL; n++)
 	{
 		if (h->str == NULL)
-			printf("[%u] %s\n", 0, "(nil)");
+			printf("[%u] %s\n", h->len, "(nil)");
 		else
 		{
 			printf("[%u] %s\n", h->len, h->str);
