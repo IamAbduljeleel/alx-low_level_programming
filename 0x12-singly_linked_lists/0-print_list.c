7d69a3138b3c93243b48f42de582d5740d8abdc0
Abduljeleel Bello <bellojeleel@yahoo.com>
2023-08-27 19:03:33 -0700

    Function that returns the number of elements in a linked

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * print_list - function that prints all the elements of a list_t list
+ * @h: The Singly linked list
+ * Return: The number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t my_nodes;
+
+	my_nodes = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		my_nodes++;
+	}
+	return (my_nodes);
+}
