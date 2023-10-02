e9f919efeee253c197b2dd7da900c41a8b8e0743
root <you@example.com>
2023-10-02 12:49:46 -0700

    Static Libraries Containing Functions

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _atoi - Returns a pointer
+ * @s: String
+ * Return: The pointer to the string
+ */
+
+int _atoi(char *s)
+{
+	if (*s)
+	{
+		return (0);
+	}
+	return (0);
+}
---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,1 @@
+The Stattic library Task
